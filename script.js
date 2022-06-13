34508a6fd62e0b6231c5b2980528d28efe2280a2
j.coes.tech@gmail.com <j.coes.tech@gmail.com>
2022-06-13 19:17:20 -0300

    variáveis1

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "João"
+let sobrenome = "Carlos"
+let anoNascimento = 2004
+let cidade = "Diadema"
+
+let idade = 2022 - anoNascimento
+
+console.log (`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`);
